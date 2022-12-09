6f565b01ed675d4ecbb30668304e00195b65c591
Jitty-04 <jittysjohn1997@gmail.com>
2022-12-09 14:25:06 +0530

    init

---
+++ src/main/java/com/nest/OnlineComplaint/OnlineComplaintApplication.java
@@ -0,0 +1,13 @@
+package com.nest.OnlineComplaint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineComplaintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineComplaintApplication.class, args);
+	}
+
+}
